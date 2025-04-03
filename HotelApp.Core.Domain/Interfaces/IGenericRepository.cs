cb558553f9876ff6b6b45f3ecc87477697797573
Broly 12 <brailynperezbatista@gmail.com>
2025-04-02 21:51:50 -0400

    Add project files.

---
+++ HotelApp.Core.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelApp.Core.Domain.Interfaces
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    /// <summary>
+    /// Generic repository interface for CRUD operations.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity.</typeparam>
+    /// <remarks>This interface defines the basic CRUD operations for a generic repository.</remarks>
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<bool> UpdateAsync(int id, T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
